9ed2e845b50256216befc96cdc5caac3a486bbdf
Lucas Lebret <lebretlucas.scolaire@gmail.com>
2021-04-03 21:12:46 +0200

    recover a fragment of K16

--- RnD.cpp
+++ RnD.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <bitset>
 using namespace std;
 #include "DifDistrTab.h"
 
@@ -48,8 +49,23 @@ string bin2hex(string s)
     mp["1101"] = "D";
     mp["1110"] = "E";
     mp["1111"] = "F";
+    mp["00"] = "0";
+    mp["01"] = "1";
+    mp["10"] = "2";
+    mp["11"] = "3";
     string hex = "";
-    for (long unsigned int i = 0; i < s.length(); i += 4) {
+    long unsigned int i = 0;
+
+    if(s.length() == 6)
+    {
+      string ch = "";
+      ch += s[i];
+      ch += s[i + 1];
+      i += 2;
+
+      hex += mp[ch];
+    }
+    for (; i < s.length(); i += 4) {
         string ch = "";
         ch += s[i];
         ch += s[i + 1];
@@ -370,6 +386,127 @@ void testDDT(int*** DDT, string SB_I[8], string SB_O[8])
   }
 }
 
+void getDifPerSBox(int*** DDT, string SB_I[8], string SB_O[8], int* difPerSBox)
+{
+  for(int i = 0; i < 8; i++)
+  {
+      difPerSBox[i] = DDT[i][bin2int(SB_I[i])][bin2int(SB_O[i])];
+  }
+}
+
+string int2bin(int a)
+{
+  string bin = bitset<6>(a).to_string();
+
+  return bin;
+
+}
+
+int getRow(string input)
+{
+  string row = "";
+  row += input[0];
+  row += input[5];
+  return bin2int(row);
+}
+
+int getColumn(string input)
+{
+  string column = "";
+  column = input.substr(1,4);
+  return bin2int(column);
+}
+
+string getViablePair(string SB_I, string SB_O, int s[4][16], int pairAmount)
+{
+  cout << "IN = " << SB_I << endl << "OUT = " << SB_O << endl;
+
+  string SI, F_SI;
+  int pairFound = 0;
+  int ct = 1;
+
+  int i = -1, j = -1;
+  int SO = -1, F_SO = -1;
+
+  int SB_O_int = bin2int(SB_O);
+  int SO_xor_F_SO = -1;
+
+  while(pairFound < pairAmount)
+  {
+
+    SI = int2bin(ct);
+    F_SI = xor_(SI, SB_I);
+    //cout << SI << " --- " << F_SI << endl;
+    //cout << "SI = " << bin2hex(SI) << " XOR " << bin2hex(F_SI) << " = " << xor_(SI, F_SI) << " ===> " << SB_I << endl << endl;
+
+    i = getRow(SI);
+    j = getColumn(SI);
+    SO = s[i][j];
+    //cout << "row = " << i <<  " // column = " << j << " --- S(SI)= " << s[i][j] << endl;
+
+    i = getRow(F_SI);
+    j = getColumn(F_SI);
+    F_SO = s[i][j];
+    //cout << "row = " << i <<  " // column = " << j << " --- S(F_SI)= " << s[i][j] << endl;
+
+    SO_xor_F_SO = SO^F_SO;
+    if(SO_xor_F_SO == SB_O_int)
+    {
+      cout << "pair found: SI = " << SI << " --- F_SI = " << F_SI << endl;
+      return SI;
+      pairFound++;
+    }
+
+    ct++;
+  }
+
+  return "-1";
+}
+
+void getKeyFragment(int* difPerSBox, string SB_I[8], string SB_O[8], string ER15[8])
+{
+  int s[8][4][16] = { { 14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,
+                        0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8,
+                        4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0,
+                        15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13 },
+                      { 15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10,
+                        3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5,
+                        0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15,
+                        13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9 },
+
+                      { 10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8,
+                        13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1,
+                        13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7,
+                        1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12 },
+                      { 7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15,
+                        13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9,
+                        10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4,
+                        3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14 },
+                      { 2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9,
+                        14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6,
+                        4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14,
+                        11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3 },
+                      { 12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11,
+                        10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8,
+                        9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6,
+                        4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13 },
+                      { 4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1,
+                        13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6,
+                        1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2,
+                        6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12 },
+                      { 13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7,
+                        1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2,
+                        7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8,
+                        2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11 } };
+
+  string Si_I;
+  Si_I = getViablePair(SB_I[1], SB_O[1], s[1], difPerSBox[1]);
+  string K16_i =xor_(Si_I, ER15[1]);
+
+  cout << K16_i << " = " << Si_I << " xor " << ER15[1] << endl;
+}
+
+
 int main()
 {
 
@@ -401,6 +538,7 @@ int main()
 
   //S_In
   R15 = expend(R15);
+  //cout << "----------------------ER15-----------------" << endl << R15 << endl;
   F_R15 = expend(F_R15);
   string SBox_In_full = xor_(R15, F_R15);
 
@@ -420,8 +558,13 @@ int main()
   DDT = allocDDT();
   load_Difference_Distribution_Tables(DDT);
 
-  testDDT(DDT, SBox_In, SBox_Out);
+  int difPerSBox[8];
 
+  getDifPerSBox(DDT, SBox_In, SBox_Out, difPerSBox);
+
+  string cutER15[8];
+  SBox_Cutting(R15, 6, cutER15);
+  getKeyFragment(difPerSBox, SBox_In, SBox_Out, cutER15);
   // unsigned long int key_prototype = (unsigned long int)18446744073709551615;
   // string key =  int_to_hex(key_prototype);
   //
